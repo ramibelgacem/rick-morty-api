5f7fffababb6b69584c8f5c737b2b02f19988961
Rami Belgacem <ramibelgacem@gmail.com>
2021-12-05 00:32:39 +0100

    [IMP] show comments in the episode get endpoint

--- app/schemas/episode.py
+++ app/schemas/episode.py
@@ -1,7 +1,9 @@
 from typing import List
 
 from app.schemas.base import CharacterBase, EpisodeBase
+from app.schemas.comment import CommentOut
 
 
 class EpisodeOut(EpisodeBase):
     characters: List[CharacterBase] = []
+    comments: List[CommentOut] = []

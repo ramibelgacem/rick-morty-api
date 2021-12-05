cc40f9e1073be90022419a6a0a8f43815314739a
Rami Belgacem <ramibelgacem@gmail.com>
2021-12-05 20:31:23 +0100

    [IMP] refactoring comment unit test

--- app/tests/test_comment.py
+++ app/tests/test_comment.py
@@ -8,32 +8,43 @@ from app.tests.database_utils import override_get_db
 app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
-COMMENT_ID = 16
 EPISODE_ID = 1
+
+new_user_response = client.post(
+    "/user/",
+    json={
+        "email": "fakeusercomment@email.com",
+        "password": "fakepassword"
+    }
+)
+created_user = new_user_response.json()
+
+new_comment_response = client.post(
+    f"/episode/{EPISODE_ID}/comment",
+    json={
+        "message": "Testing a new comment",
+        "user_id": created_user['id']
+    }
+)
+new_comment = new_comment_response.json()
 
 
 def test_update_comment():
     response = client.put(
-        f"/comment/{COMMENT_ID}",
+        f"/comment/{new_comment['id']}",
         json={
             "message": "Updating a comment"
         }
     )
 
     assert response.status_code == status.HTTP_202_ACCEPTED, response.text
-    created_comment = response.json()
-    assert created_comment["message"] == "Updating a comment"
+    updated_comment = response.json()
+    assert updated_comment["message"] == "Updating a comment"
 
 
 def test_delete_comment():
-    response = client.post(
-        f"/episode/{EPISODE_ID}/comment",
-        json={
-            "message": "This comment will be deleted"
-        }
-    )
-    assert response.status_code == status.HTTP_201_CREATED, response.text
-    created_comment = response.json()
+    assert new_comment_response.status_code == \
+        status.HTTP_201_CREATED, new_comment_response.text
 
-    response = client.delete(f"/comment/{created_comment['id']}")
+    response = client.delete(f"/comment/{new_comment['id']}")
     assert response.status_code == status.HTTP_200_OK, response.text

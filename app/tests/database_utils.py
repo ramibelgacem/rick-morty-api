4a0b4426c566d379d943d9f5d8292a09c8873f4e
Rami Belgacem <ramibelgacem@gmail.com>
2021-12-05 16:57:05 +0100

    [IMP] unit test database parameters

--- app/tests/database_utils.py
+++ app/tests/database_utils.py
@@ -1,11 +1,18 @@
-from fastapi.testclient import TestClient
+from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.database import Base
 
-SQLALCHEMY_DATABASE_URL = \
-    "postgresql://odoo:postgres@localhost:5432/rick_morty"
+DATABASE_USER = config('DATABASE_USER')
+DATABASE_PASSWORD = config('DATABASE_PASSWORD')
+DATABASE_HOST = config('DATABASE_HOST', default='localhost')
+DATABASE_PORT = config('DATABASE_PORT', default='5432')
+DATABASE_NAME = config('DATABASE_NAME')
+
+SQLALCHEMY_DATABASE_URL =  \
+    f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:\
+    {DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL

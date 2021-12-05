adec14e44950ee9254595edfe61b2f152aadb326
Rami Belgacem <ramibelgacem@gmail.com>
2021-12-05 16:52:48 +0100

    [IMP] configure postgresql parameters with environment variables

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # rick-morty-api
 
+# Requirement
+python3.9+
+
+# ENv variables for postgresql connection
+Create a file .env under rick-morty-api directory and put these variables with your values
+DATABASE_USER=<your_database_user>
+DATABASE_PASSWORD=<your_database_password>
+DATABASE_HOST=<your_database_host>
+DATABASE_PORT=<your_database_port>
+DATABASE_NAME=<your_database_name>
+
 pip install -r requirements.txt
 
 uvicorn app.main:app --reload
--- app/database.py
+++ app/database.py
@@ -1,9 +1,19 @@
+from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
+DATABASE_USER = config('DATABASE_USER')
+DATABASE_PASSWORD = config('DATABASE_PASSWORD')
+DATABASE_HOST = config('DATABASE_HOST', default='localhost')
+DATABASE_PORT = config('DATABASE_PORT', default='5432')
+DATABASE_NAME = config('DATABASE_NAME')
+
+
 SQLALCHEMY_DATABASE_URL = \
-    "postgresql://odoo:postgres@localhost:5432/rick_morty"
+    f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:\
+        {DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
